4ee5b98262da73581f7d9fbaef333a20b9ed4989
Michael Pierre-Louis <82857756+mgpierrelouis@users.noreply.github.com>
2021-10-01 10:11:33 -0400

    updated book model to include description, link and image

--- models/book.js
+++ models/book.js
@@ -2,10 +2,25 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  title: { 
+      type: String, 
+      required: true 
+    },
+  authors: { 
+      type: String, 
+      required: true 
+    },
+  description: { 
+      type: String,
+      required: true 
+    },
+  image: {
+      type: String,
+  },
+  link: {
+      type: String,
+
+  }
 });
 
 const Book = mongoose.model("Book", bookSchema);
