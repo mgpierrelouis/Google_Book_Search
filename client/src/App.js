04734ac474331c729032a5166d62dd4605406d89
Michael Pierre-Louis <82857756+mgpierrelouis@users.noreply.github.com>
2021-10-01 08:54:17 -0400

    setup app.js to render components

--- client/src/App.js
+++ client/src/App.js
@@ -4,3 +4,26 @@ import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+
+function App() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path={["/", "/books"]}>
+              <Books />
+            </Route>
+            <Route exact path="/books/:id">
+              <Detail />
+            </Route>
+            <Route>
+              <NoMatch />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+  
+  export default App;
