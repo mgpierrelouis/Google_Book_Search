38d8355d0f3e5bba1acf0a137e527faf23dc4b96
Michael Pierre-Louis <82857756+mgpierrelouis@users.noreply.github.com>
2021-10-01 08:34:50 -0400

    setup detail page imports

---
+++ client/src/pages/Detail.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
